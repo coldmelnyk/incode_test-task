8f3701b063b50c0c8ac2b7f84e08f45b39c2b569
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-03-02 19:48:38 +0200

    changed token for fetching from gh api

--- src/utils/fetchIssues.ts
+++ src/utils/fetchIssues.ts
@@ -1,6 +1,6 @@
 export const fetchIssues = async (path: string) => {
   const URL = `https://api.github.com/repos/${path}/issues`;
-  const token = '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';
+  const token = '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';
 
   return await fetch(URL, {
     headers: { Authorization: `Bearer ${token}` }
