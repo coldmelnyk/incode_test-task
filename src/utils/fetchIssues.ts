a8c144c5e1d0e9a29e054649d3f7523988c9234f
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-03-02 20:00:36 +0200

    fixed auth with token

--- src/utils/fetchIssues.ts
+++ src/utils/fetchIssues.ts
@@ -3,7 +3,7 @@ export const fetchIssues = async (path: string) => {
   const token = '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';
 
   return await fetch(URL, {
-    headers: { Authorization: `Bearer ${token}` }
+    headers: { Authorization: `${token}` }
   })
     .then(res => res.json())
     .catch(() => alert('Failed in fetchIssues'));
